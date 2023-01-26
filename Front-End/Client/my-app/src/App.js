bd023a4dd028be0412fbc8c18a8ae72ddafb5a79
lydiaBn <94543559+lydiaBn@users.noreply.github.com>
2023-01-26 14:10:29 +0100

    Starting from Scratch

---
+++ Front-End/Client/my-app/src/App.js
@@ -0,0 +1,31 @@
+import "./App.css";
+
+function App() {
+  return (
+    <main>
+      <header>
+        <nav>
+          <div class="navbar1">
+            <a href="" className="logo">
+              <img
+                src="https://i.pinimg.com/736x/86/b1/e9/86b1e9eb251904fe9126e72e0f0f19eb.jpg"
+                alt="logo"
+              />
+            </a>
+            <a href=""> Accueil </a>
+            <a href=""> EtatCivil </a>
+            <a href=""> Statistique </a>
+            <a href=""> PlanDuSite</a>
+            <a href=""> Contact </a>
+          </div>
+          <div class="navbar2">
+            <a href=""> Connexion </a>
+            <a href=""> S'inscrire </a>
+          </div>
+        </nav>
+      </header>
+    </main>
+  );
+}
+
+export default App;
